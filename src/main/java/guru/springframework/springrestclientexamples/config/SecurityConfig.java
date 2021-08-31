c0d6251782ce9a6ad5a50e166179e160e324ace8
Thiago Cavalcante <thiago.kun@gmail.com>
2021-08-31 02:45:48 -0300

    Prepare app for deployment on Heroku
    According to a post on Heroku Dev Center.
    
    Changes:
    - Force the use of HTTPS with the SecurityConfig
    - Specify a Java version for Heroku using system.properties file
    - Add Heroku Maven Plugin to pom

--- src/main/java/guru/springframework/springrestclientexamples/config/SecurityConfig.java
+++ src/main/java/guru/springframework/springrestclientexamples/config/SecurityConfig.java
@@ -10,7 +10,15 @@ public class SecurityConfig {
 
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(final ServerHttpSecurity http) {
-        http.authorizeExchange().anyExchange().permitAll().and().csrf().disable();
+        http.redirectToHttps()
+            .httpsRedirectWhen(e -> e.getRequest().getHeaders().containsKey("X-Forwarded-Proto"))
+            .and()
+            .authorizeExchange()
+            .anyExchange()
+            .permitAll()
+            .and()
+            .csrf()
+            .disable();
 
         return http.build();
     }
